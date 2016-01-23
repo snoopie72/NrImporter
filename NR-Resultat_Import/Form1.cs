3ec4423bb624fd1fb7b894fae4b6a57932d96b9f
kaihugo <khugos@gmail.com>
2016-01-23 20:50:27 +0100

    importknapp

---
+++ NR-Resultat_Import/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NR_Resultat_Import
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.DefaultExt = ".csv";
+            openFileDialog1.ShowDialog();
+            
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            textBox1.Text = openFileDialog1.FileName;
+        }
+    }
+}
